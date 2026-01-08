0b8028cd6fbb91a238e3edf3d9e44e756f265a65
Shehan <ertsshehan@gmail.com>
2026-01-08 23:29:56 +0530

    Update API base URL to production environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios, { AxiosError } from "axios";
 import { refreshTokens } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://ugliest-alexina-shehanproject-b5ec33ba.koyeb.app/api/v1",
 });
 
 const PUBLIC_ENDPOINTS = ["/auth/login", "/auth/register-doctor"];
